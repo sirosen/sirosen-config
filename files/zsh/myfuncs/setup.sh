46e110d99d9f309e031a3d727fa477877451d0b2
Stephen Rosen <sirosen@uchicago.edu>
2014-12-10 11:05:30 -0600

    Fix to solarized gconf setup
    Some elements of the colorscheme were written incorrectly for gconf, and
    resulted in an almost solarized colorscheme.

--- files/zsh/myfuncs/setup.sh
+++ files/zsh/myfuncs/setup.sh
@@ -6,22 +6,22 @@ vundle-setup () {
 
 magick-hostname-background-gnome3 () {
     local image_loc="$HOME/Pictures/autogenbg.png"
-    convert -size 1440x900 xc:lightgrey -font Palatino-Bold -pointsize 128 -fill black -draw "text 400,300 '`hostname`'" -blur 0x1 "$image_loc"
-    gsettings set org.gnome.desktop.background picture-uri file:///"$image_loc"
-    gsettings set org.gnome.desktop.background picture-options "stretched"
+    convert -size 1440x900 xc:lightgrey -font Palatino-Bold -pointsize 128 -fill black -draw "text 400,300 '$(hostname)'" -blur 0x1 "$image_loc"
+    gsettings set "org.gnome.desktop.background picture-uri" "file:///$image_loc"
+    gsettings set "org.gnome.desktop.background picture-options" "stretched"
 }
 
 solarized-terminal-setup () {
     case "$1" in
         "dark")
-            PALETTE="#070736364242:#D3D301010202:#858599990000:#B5B589890000:#26268B8BD2D2:#D3D336368282:#2A2AA1A19898:#EEEEE8E8D5D5:#00002B2B3636:#CBCB4B4B1616:#58586E6E7575:#65657B7B8383:#838394949696:#6C6C7171C4C4:#9393A1A1A1A1:#FDFDF6F6E3E3"
+            PALETTE="#070736364242:#DCDC32322F2F:#858599990000:#B5B589890000:#26268B8BD2D2:#D3D336368282:#2A2AA1A19898:#EEEEE8E8D5D5:#00002B2B3636:#CBCB4B4B1616:#58586E6E7575:#65657B7B8383:#838394949696:#6C6C7171C4C4:#9393A1A1A1A1:#FDFDF6F6E3E3"
             BG_COLOR="#00002B2B3636"
-            FG_COLOR="#65657B7B8383"
+            FG_COLOR="#838394949696"
             ;;
         "light")
-            PALETTE="#EEEEE8E8D5D5:#D3D301010202:#858599990000:#B5B589890000:#26268B8BD2D2:#D3D336368282:#2A2AA1A19898:#070736364242:#FDFDF6F6E3E3:#CBCB4B4B1616:#9393A1A1A1A1:#838394949696:#65657B7B8383:#6C6C7171C4C4:#58586E6E7575:#00002B2B3636"
+            PALETTE="#070736364242:#DCDC32322F2F:#858599990000:#B5B589890000:#26268B8BD2D2:#D3D336368282:#2A2AA1A19898:#EEEEE8E8D5D5:#00002B2B3636:#CBCB4B4B1616:#58586E6E7575:#65657B7B8383:#838394949696:#6C6C7171C4C4:#9393A1A1A1A1:#FDFDF6F6E3E3"
             BG_COLOR="#FDFDF6F6E3E3"
-            FG_COLOR="#838394949696"
+            FG_COLOR="#65657B7B8383"
             ;;
         *)
             echo "Usage: solarize [light | dark]"
@@ -35,10 +35,15 @@ solarized-terminal-setup () {
     gconftool-2 --set "/apps/gnome-terminal/profiles/Default/background_color" --type string "$BG_COLOR"
     gconftool-2 --set "/apps/gnome-terminal/profiles/Default/foreground_color" --type string "$FG_COLOR"
 }
-solarized-light-terminal-setup () { solarized-terminal-setup light }
-solarized-dark-terminal-setup () { solarized-terminal-setup dark }
+solarized-light-terminal-setup () {
+    solarized-terminal-setup light
+}
+solarized-dark-terminal-setup () {
+    solarized-terminal-setup dark
+}
 
 debian-install-shellcheck () {
+    mkdir -p ~/bin;
     cd ~/bin;
     sudo apt-get install ghc libghc-parsec3-dev libghc-json-dev libghc-regex-compat-dev libghc-quickcheck2-dev pandoc;
     git clone git@github.com:koalaman/shellcheck.git;
