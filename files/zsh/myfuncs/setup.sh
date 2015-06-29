40bdd3009ab6dd7167bbeed6f407674967866b52
Stephen Rosen <sirosen@uchicago.edu>
2015-06-29 15:13:42 -0500

    Switch solarization config to xterm
    Switching over to xfce4-term, ditch the gnome terminal solarization

--- files/zsh/myfuncs/setup.sh
+++ files/zsh/myfuncs/setup.sh
@@ -12,28 +12,7 @@ magick-hostname-background-gnome3 () {
 }
 
 solarized-terminal-setup () {
-    case "$1" in
-        "dark")
-            PALETTE="#070736364242:#DCDC32322F2F:#858599990000:#B5B589890000:#26268B8BD2D2:#D3D336368282:#2A2AA1A19898:#EEEEE8E8D5D5:#00002B2B3636:#CBCB4B4B1616:#58586E6E7575:#65657B7B8383:#838394949696:#6C6C7171C4C4:#9393A1A1A1A1:#FDFDF6F6E3E3"
-            BG_COLOR="#00002B2B3636"
-            FG_COLOR="#838394949696"
-            ;;
-        "light")
-            PALETTE="#070736364242:#DCDC32322F2F:#858599990000:#B5B589890000:#26268B8BD2D2:#D3D336368282:#2A2AA1A19898:#EEEEE8E8D5D5:#00002B2B3636:#CBCB4B4B1616:#58586E6E7575:#65657B7B8383:#838394949696:#6C6C7171C4C4:#9393A1A1A1A1:#FDFDF6F6E3E3"
-            BG_COLOR="#FDFDF6F6E3E3"
-            FG_COLOR="#65657B7B8383"
-            ;;
-        *)
-            echo "Usage: solarize [light | dark]"
-            exit
-            ;;
-    esac
-
-    gconftool-2 --set "/apps/gnome-terminal/profiles/Default/use_theme_background" --type bool false
-    gconftool-2 --set "/apps/gnome-terminal/profiles/Default/use_theme_colors" --type bool false
-    gconftool-2 --set "/apps/gnome-terminal/profiles/Default/palette" --type string "$PALETTE"
-    gconftool-2 --set "/apps/gnome-terminal/profiles/Default/background_color" --type string "$BG_COLOR"
-    gconftool-2 --set "/apps/gnome-terminal/profiles/Default/foreground_color" --type string "$FG_COLOR"
+    cp "$HOME/.config/xfce4/terminal/$1" "$HOME/.config/xfce4/terminal/terminalrc"
 }
 solarized-light-terminal-setup () {
     solarized-terminal-setup light
